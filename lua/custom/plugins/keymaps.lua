c142e2650f37c9763b92f9d873a1e948beea316b
himchawla <58020143+himchawla@users.noreply.github.com>
2024-09-03 03:36:08 +0530

    Changes 3 Sept

---
+++ lua/custom/plugins/keymaps.lua
@@ -0,0 +1,20 @@
+return {
+  -- Global keymaps
+  -- These are keymaps that are global and are not specific to the plugin
+  -- and are not part of the plugin
+
+  -- Open the telescope
+  -- This is a global keymap that is not specific to the plugin
+  -- cmnd+/ to comment
+
+  -- leader + T to toggle Neotree
+  vim.api.nvim_set_keymap('n', '<leader>T', ':Neotree<CR>', { noremap = true, silent = true }),
+  -- return to accept the selection instead of <C-y>
+
+  require('cmp').setup {
+    mapping = {
+      ['<CR>'] = require('cmp').mapping.confirm { select = true },
+    },
+  },
+  -- vim.api.nvim_set_keymap('n', '<CR>', ':Telescope send_selected_to_qflist<CR>', { noremap = true, silent = true }),
+}
